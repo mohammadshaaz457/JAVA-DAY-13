d17ee08bae7695ebf39f54e3fa108d0f18002b3d
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-14 11:29:59 +0530

    Add files via upload

---
+++ singleinheritence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class a
+{
+    public void a()
+    {
+        System.out.println("hello");
+    }
+    public void b()
+    {
+        System.out.println(" Ashraf");
+     }
+}
+public class singleinheritence
+{
+    public static void main(String args[])
+    {
+        a s=new a();
+        s.a();
+        s.b();
+        
+    }
+}
