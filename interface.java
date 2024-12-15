2afe119e039db3a0feb9258705cc9783436186a5
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-15 11:00:25 +0530

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,31 @@
+class MyRunnable implements Runnable {
+    private String name;
+    
+    public MyRunnable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + " is running.");
+        try {
+            Thread.sleep(1000);  // Simulate some work
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(name + " finished.");
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyRunnable task1 = new MyRunnable("Thread 1");
+        MyRunnable task2 = new MyRunnable("Thread 2");
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+
+        thread1.start();
+        thread2.start();
+    }
+}
